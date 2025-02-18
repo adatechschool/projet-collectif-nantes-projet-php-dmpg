7677c4c8e9a414f0ab27f1b7a9d4238bc8bae229
Molid <delmolid@hotmail.fr>
2025-02-18 17:40:42 +0100

    accessibilité changement de couleur sur le button

--- src/php/php/collection_add.php
+++ src/php/php/collection_add.php
@@ -109,7 +109,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
                 <!-- Boutons -->
                 <div class="flex justify-end space-x-4">
                     <a href="collection_list.php" class="bg-gray-500 hover:bg-gray-600 text-white px-4 py-2 rounded-lg shadow">Annuler</a>
-                    <button type="submit" class="bg-cyan-200 hover:bg-cyan-600 text-white px-4 py-2 rounded-lg shadow">
+                    <button type="submit" class="border border-solid border-cyan-500 border-blue-500 hover:bg-cyan-600 text-black px-4 py-2 rounded-lg shadow ai-style-change-1" aria-label="Ajouter un nouvel élément">
                         ➕ Ajouter
                     </button>
                 </div>
--- src/php/php/header.php
+++ src/php/php/header.php
@@ -1,15 +1,16 @@
- <!-- Barre de navigation -->
- <div class="bg-blue-800 text-white w-64 p-6">
-        <h2 class="text-2xl font-bold mb-6">Dashboard</h2>
-            <a href="collection_list.php" class="flex items-center py-2 px-3 hover:border border-solid rounded-lg"><i class="fas fa-tachometer-alt mr-3"></i> Accueil</a>
-            <a href="collection_list.php" class="flex items-center py-4 px-3 hover:border border-solid rounded-lg"><i class="fa-solid fa-list mr-3"></i> Liste des collectes</a>
-            <a href="volunteer_list.php" class="flex items-center py-4 px-3 hover:border border-solid rounded-lg"><i class="fa-solid fa-clipboard-list mr-4"></i> Liste des bénévoles</a>
-            <a href="collection_add.php" class="flex items-center py-4 px-3 hover:border border-solid rounded-lg"><i class="fas fa-plus-circle mr-3"></i> Ajouter une collecte</a>
-            <a href="user_add.php" class="flex items-center py-4 px-3 hover:border border-solid rounded-lg"><i class="fas fa-user-plus mr-2.5"></i> Ajouter un bénévole</a>
-            <a href="my_account.php" class="flex items-center py-4 px-3 hover:border border-solid rounded-lg"><i class="fas fa-cogs mr-2.5"></i> Mon compte</a>
-        <div class="mt-6">
-            <button onclick="logout()" class="w-full bg-red-600 hover:bg-red-700 text-white py-4 rounded-lg shadow-md">
-                Déconnexion
-            </button>
-        </div>
-</div>
+    <!-- Barre de navigation -->
+    <div class="bg-blue-800 text-white w-64 p-6">
+            <h2 class="text-2xl font-bold mb-6">Dashboard</h2>
+            
+            <button> <a href="collection_list.php" class="flex items-center py-2 px-3 hover:border border-solid rounded-lg"><i class="fas fa-tachometer-alt mr-3"></i> Accueil </a> </button>
+            <button> <a href="collection_list.php" class="flex items-center py-4 px-3 hover:border border-solid rounded-lg"><i class="fa-solid fa-list mr-3"></i> Liste des collectes</a> </button>
+            <button> <a href="volunteer_list.php" class="flex items-center py-4 px-3 hover:border border-solid rounded-lg"><i class="fa-solid fa-clipboard-list mr-4"></i> Liste des bénévoles</a> </button>
+            <button> <a href="collection_add.php" class="flex items-center py-4 px-3 hover:border border-solid rounded-lg"><i class="fas fa-plus-circle mr-3"></i> Ajouter une collecte</a> </button>
+            <button> <a href="user_add.php" class="flex items-center py-4 px-3 hover:border border-solid rounded-lg"><i class="fas fa-user-plus mr-2.5"></i> Ajouter un bénévole</a> </button>
+            <button> <a href="my_account.php" class="flex items-center py-4 px-3 hover:border border-solid rounded-lg"><i class="fas fa-cogs mr-2.5"></i> Mon compte</a> </button>
+            <div class="mt-6">
+                <button onclick="logout()" class="w-full bg-red-600 hover:bg-red-700 text-white py-4 rounded-lg shadow-md">
+                    Déconnexion
+                </button>
+            </div>
+    </div>
